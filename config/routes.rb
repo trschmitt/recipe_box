175a6bdc894ceb94094e23cf4d02abb5416828df
Tristan Schmitt <trschmitt50@live.com>
2017-09-22 14:54:36 -0400

    Added new recipe form and index page views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipes/create'
+  groot to: 'recipes#index'
 
-  get 'recipes/index'
-
-  get 'recipes/show'
+  resources :recipes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
